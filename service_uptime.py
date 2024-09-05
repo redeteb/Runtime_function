be78fd643ffc07e8a776ce86e290dd9d1d476ee7
Redete Bezabeh <134902499+redeteb@users.noreply.github.com>
2024-09-05 17:19:12 -0500

    Create service_uptime.py
    initial commit

---
+++ service_uptime.py
@@ -0,0 +1,15 @@
+# Calculate uptime percentage of a service
+# Take 2 parameters, Total Hours and Down Hours
+# Return uptime percentage rounded to two decimal places 
+
+
+total_hours = 6
+down_hours = .32
+
+
+def service_uptime():
+    uptime = (total_hours - down_hours) / total_hours
+    print(f"The service's uptime is {uptime:.2%}.")
+
+service_uptime()
+
